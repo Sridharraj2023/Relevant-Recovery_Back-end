e7d90ae44e05ea95d50e4df4a110bc335e431e37
Sridharraj2023 <sridharraj0110@gmail.com>
2025-07-15 22:59:06 +0530

    Image uploads updated

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDb = require('./config/db');
+const path = require('path');
 
 // Load environment variables
 dotenv.config();
@@ -12,6 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
