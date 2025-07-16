fe2bf9e8f4ca8dd959601ebbaf0b75629504a150
Sridharraj2023 <sridharraj0110@gmail.com>
2025-07-16 21:23:54 +0530

    Image Upload directory updaed in Server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads/events', express.static(path.join(__dirname, 'uploads/events')));
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
